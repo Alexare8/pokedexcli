b32d39f838c914ced045783125b9036400cc9ea0
alexare8 <duncorr@gmail.com>
2026-07-22 03:47:22 -0400

    CH1 Refactored to match solution files

--- repl.go
+++ repl.go
@@ -7,16 +7,42 @@ import (
 	"strings"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commands := getCommands()
+		command, exists := commands[words[0]]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
 }
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
 }
 
 func getCommands() map[string]cliCommand {
@@ -31,46 +57,10 @@ func getCommands() map[string]cliCommand {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
 	}
-}
-
-func startRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-
-		words := cleanInput(scanner.Text())
-		if len(words) == 0 {
-			continue
-		}
-
-		commands := getCommands()
-		command, exists := commands[words[0]]
-		if !exists {
-			fmt.Println("Unknown command")
-			continue
-		}
-		callback := command.callback
-		err := callback()
-		if err != nil {
-			fmt.Errorf("Error: %v", err)
-		}
-	}
-}
-
-func commandExit() error {
-	fmt.Print("Closing the Pokedex... Goodbye!")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("Usage:\n")
-	commands := getCommands()
-	for _, command := range commands {
-		fmt.Printf("%v: %v\n", command.name, command.description)
-	}
-	return nil
 }
